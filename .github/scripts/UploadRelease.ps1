a4080efbd8b682a72d4feadf84fb107bad69149f
Eivind S. Antonsen <esa@telia.no>
2024-02-26 10:49:36 +0100

    Update zip creation method in UploadRelease.ps1
    The zip file creation method in UploadRelease.ps1 has been updated. Instead of using the CreateFromDirectory method, the script now uses the Open method from System.IO.Compression.ZipFile to create a zip entry for the given executable file. Also, redundant reading of file bytes has been removed.

--- .github/scripts/UploadRelease.ps1
+++ .github/scripts/UploadRelease.ps1
@@ -6,12 +6,21 @@
     [String]$version
 )
 
-$dirPath = "${workspace}/Staging/Build/"
-$zipFilePath = "${workspace}/Staging/Build/DetectiveSpecs-v${version}.exe"
-[System.IO.Compression.ZipFile]::CreateFromDirectory($dirPath, $zipFilePath)
+Add-Type -Assembly System.IO.Compression.FileSystem
+
+$exeFilePath = "${workspace}\Staging\Build\DetectiveSpecs-v${version}.exe"
+$zipFilePath = "${workspace}\Staging\Build\DetectiveSpecs-v${version}.zip"
+if (Test-Path $zipFilePath){ Remove-Item $zipFilePath }
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+$includeBaseDirectory = $false
+$zip = [System.IO.Compression.ZipFile]::Open($zipFilePath, 'Update')
+$relativePath = [System.IO.Path]::GetRelativePath((Get-Location).Path, $exeFilePath)
+$entry = $zip.CreateEntryFromFile($exeFilePath, $relativePath, $compressionLevel)
+$zip.Dispose()
+
 $assetFileName = [System.IO.Path]::GetFileName($zipFilePath)
 $uri = "https://uploads.github.com/repos/${repository}/releases/${releaseId}/assets?name=${assetFileName}"
-
+  
 $headers = @{
     "Accept" = "application/vnd.github+json"
     "Authorization" = "Bearer $accessToken"
@@ -19,6 +28,5 @@ $headers = @{
     "Content-Type" = "application/octet-stream"
 }
 
-$fileBytes = [System.IO.File]::ReadAllBytes($assetFilePath)
-
+$fileBytes = [System.IO.File]::ReadAllBytes($zipFilePath)
 Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body $fileBytes
