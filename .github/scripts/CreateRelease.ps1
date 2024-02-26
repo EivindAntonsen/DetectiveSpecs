f09db741d6b68c97dc771f3ce830535645354b3d
Eivind Antonsen <eivind.antonsen@gmail.com>
2024-02-26 03:29:18 +0100

    Add logging for release body in CreateRelease script
    The change introduces a logging line to the CreateRelease PowerShell script. This facilitates easier debugging by displaying the body parameter value before the Invoke-RestMethod is called.

--- .github/scripts/CreateRelease.ps1
+++ .github/scripts/CreateRelease.ps1
@@ -17,4 +17,6 @@ $body = @{
 $uri = "https://api.github.com/repos/${repository}/releases"
 $accept = "application/vnd.github+json"
 
+Write-Host "Body is $body"
+
 Invoke-RestMethod -Uri $uri -Method Post -Headers @{"Accept" = $accept; "Authorization" = "Bearer $accessToken"; "X-GitHub-Api-Version" = "2022-11-28"} -Body $body
