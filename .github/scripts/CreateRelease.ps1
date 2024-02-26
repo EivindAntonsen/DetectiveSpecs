917a1d4f31d56ac8eb9dd7fb37849c9f7087d0de
Eivind Antonsen <eivind.antonsen@gmail.com>
2024-02-26 03:48:16 +0100

    Update tag_name syntax in CreateRelease.ps1
    The previous tag_name syntax in the CreateRelease.ps1 script was corrected. This change updates the construction of the tag_name to be generated by concatenating "v" with the $version variable. This ensures accurate and consistent version tagging for all future releases.

--- .github/scripts/CreateRelease.ps1
+++ .github/scripts/CreateRelease.ps1
@@ -5,7 +5,7 @@
 )
 
 $body = @{
-    tag_name = "v$version"
+    tag_name = "v" + $version
     target_commitish = "master"
     name = $version
     body = "Latest automated release of Detective Specs"
