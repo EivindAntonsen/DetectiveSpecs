93258a31f19922419a2717584cc309f94482cc46
Eivind Antonsen <eivind.antonsen@gmail.com>
2024-02-27 09:34:56 +0100

    Add output of request body in CreateRelease script
    The change in the CreateRelease.ps1 script allows for the request body to be output. This is beneficial for debugging and ensures transparency of the data being sent in the API request.

--- .github/scripts/CreateRelease.ps1
+++ .github/scripts/CreateRelease.ps1
@@ -17,5 +17,6 @@ $uri = "https://api.github.com/repos/DetectiveSpecs/releases"
 $accept = "application/vnd.github+json"
 
 Write-Host $uri
+Write-Host $body
 
 Invoke-RestMethod -Uri $uri -Method Post -Headers @{"Accept" = $accept; "Authorization" = "Bearer $accessToken"; "X-GitHub-Api-Version" = "2022-11-28"} -Body $body
