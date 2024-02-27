8597573485b75f685109db13390b2a4481cbb4cb
Eivind Antonsen <eivind.antonsen@gmail.com>
2024-02-27 09:43:52 +0100

    Update release script with header information
    The CreateRelease.ps1 script has been updated to include additional header information and generate release notes. A new boolean value, 'make_latest', has been added to keep the latest release up-to-date. Furthermore, the Invoke-RestMethod has been adjusted to incorporate the new header information.

--- .github/scripts/CreateRelease.ps1
+++ .github/scripts/CreateRelease.ps1
@@ -6,17 +6,23 @@
 $body = @{
     tag_name = "v" + $version
     target_commitish = "master"
-    name = $version
-    body = "Latest automated release of Detective Specs"
     draft = $false
     prerelease = $false
-    generate_release_notes = $false
+    generate_release_notes = $true,
+    make_latest = $true
 } | ConvertTo-Json
+
+$headers = @{
+    "Accept" = $accept; 
+    "Authorization" = "Bearer $accessToken"; 
+    "X-GitHub-Api-Version" = "2022-11-28"
+}
 
 $uri = "https://api.github.com/repos/DetectiveSpecs/releases"
 $accept = "application/vnd.github+json"
 
 Write-Host $uri
 Write-Host $body
+Write-Host $headers
 
-Invoke-RestMethod -Uri $uri -Method Post -Headers @{"Accept" = $accept; "Authorization" = "Bearer $accessToken"; "X-GitHub-Api-Version" = "2022-11-28"} -Body $body
+Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body $body
