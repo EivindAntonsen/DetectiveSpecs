652750ef9fd8b9b66749e69eb3da2ece1742b8d6
Eivind S. Antonsen <esa@telia.no>
2024-05-13 13:14:10 +0200

    Add PropertyValueFormatter to format component properties
    This commit involves the creation of a new PropertyValueFormatter class. This class contains static methods for formatting different component properties, like CPU max clock speed, GPU adapter RAM, refresh rate, video mode, storage and memory size, and memory speed, into more readable formats. This enhances the readability and understanding of the hardware components' specifications.

---
+++ DetectiveSpecs/PropertyValueFormatter.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+using DetectiveSpecs.Enums;
+
+namespace DetectiveSpecs;
+
+public static class PropertyValueFormatter
+{
+    public static object? Format(ComponentType componentType, ComponentProperty componentProperty, string? propertyValue) => componentType switch
+    {
+        ComponentType.Cpu when componentProperty is ComponentProperty.MaxClockSpeed && long.TryParse(propertyValue, out var clockSpeedHertz) =>
+            clockSpeedHertz / 1000 + " Ghz",
+        ComponentType.Gpu when componentProperty is ComponentProperty.AdapterRAM && long.TryParse(propertyValue, out var adapterRamBytes) =>
+            Math.Round(Convert.ToDouble(adapterRamBytes) / 1024 / 1024 / 1024) + " GB",
+        ComponentType.Gpu when componentProperty is ComponentProperty.MinRefreshRate or ComponentProperty.MaxRefreshRate =>
+            propertyValue + " Hz",
+        ComponentType.Gpu when componentProperty is ComponentProperty.VideoModeDescription && propertyValue is not null =>
+            GetFormattedVideoMode(propertyValue),
+        ComponentType.Storage when componentProperty is ComponentProperty.Size && long.TryParse(propertyValue, out var storageBytes) =>
+            Math.Round(Convert.ToDouble(storageBytes) / 1024 / 1024 / 1024) + " GB",
+        ComponentType.Memory when componentProperty is ComponentProperty.Capacity && long.TryParse(propertyValue, out var capacityBytes) =>
+            Math.Round(Convert.ToDouble(capacityBytes) / 1024 / 1024 / 1024) + " GB",
+        ComponentType.Memory when componentProperty is ComponentProperty.Speed && long.TryParse(propertyValue, out var memorySpeedHertz) =>
+            memorySpeedHertz / 1000 + " Ghz",
+        _ => propertyValue
+    };
+
+
+
+    private static string GetFormattedAmount(long l) => l > 1_000_000_000 ? l / 1_000_000_000 + "b " : l / 1_000_000 + "m ";
+
+
+
+    private static string GetFormattedVideoMode(string videoMode)
+    {
+        var regex = new Regex(@"\d+\s+(?=colors$)");
+
+        Match match = regex.Match(videoMode);
+        if (!long.TryParse(match.Value, out var longValue))
+            return string.Empty;
+
+        var actualAmountOfColors = longValue / 256;
+        var formattedAmount = GetFormattedAmount(actualAmountOfColors);
+
+        return regex.Replace(videoMode, formattedAmount);
+    }
+}
