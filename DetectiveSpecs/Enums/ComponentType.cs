07ccef9cf792c7b764fd8d7e5814cea0c38eedba
Eivind S. Antonsen <esa@telia.no>
2024-05-13 13:13:53 +0200

    Refactor ComponentProperty enum and adjust ComponentType properties
    The ComponentProperty enum has been refactored to better represent available properties. This gives a more exhaustive list, enabling us to retrieve more detailed information about different components. Correspondingly, the GetPropertyNames method of the ComponentType has been updated to return these improved details.

--- DetectiveSpecs/Enums/ComponentType.cs
+++ DetectiveSpecs/Enums/ComponentType.cs
@@ -22,14 +22,14 @@ public static class ComponentExtensions
     public static IEnumerable<ComponentProperty> GetPropertyNames(this ComponentType componentType) => componentType switch
     {
         Motherboard => [Name, Manufacturer, Product, SerialNumber],
-        Gpu => [Name, AdapterRAM],
-        Cpu => [Name, AdapterRAM],
-        Storage => [Model, ComponentProperty.Type, Partitions, Size],
-        Memory => [Manufacturer, Model, PartNumber, Capacity, Speed],
+        Gpu => [Name, MinRefreshRate, MaxRefreshRate, AdapterRAM, VideoModeDescription],
+        Cpu => [Name, AdapterRAM, MaxClockSpeed, NumberOfCores],
+        Storage => [Description, MediaType, Model, ComponentProperty.Type, Partitions, Size],
+        Memory => [DeviceLocator, Manufacturer, Model, PartNumber, Capacity, Speed, SerialNumber],
         Optical => [Drive, Manufacturer, MediaType, Status],
         Network => [Name, Description, DeviceID, MACAddress, PhysicalAdapter],
         Sound => [Name, Manufacturer],
-        Keyboard => [Name, Description, Layout],
+        Keyboard => [Name, Description, Layout, NumberOfFunctionKeys],
         Mouse => [Name, Manufacturer, PointerType],
         _ => throw new ArgumentOutOfRangeException(nameof(componentType), componentType, null)
     };
